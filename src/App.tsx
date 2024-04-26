021084679fc9ac9ff0f8e7808ab705874421d3cf
DawidPolanski <dpolanski1999@gmail.com>
2024-04-26 13:08:24 +0200

    Add common matches display

--- src/App.tsx
+++ src/App.tsx
@@ -3,50 +3,71 @@ import axios from "axios";
 
 function App() {
   const [summonerName, setSummonerName] = useState("");
+  const [opponentName, setOpponentName] = useState("");
   const [summonerData, setSummonerData] = useState(null);
-  const [matches, setMatches] = useState([]);
-  const [specificMatch, setSpecificMatch] = useState(null);
+  const [opponentData, setOpponentData] = useState(null);
+  // const [summonerMatches, setSummonerMatches] = useState([]);
+  // const [opponentMatches, setOpponentMatches] = useState([]);
+  const [commonMatches, setCommonMatches] = useState([]);
   const [error, setError] = useState(null);
 
-  const handleInputChange = (event) => {
+  const handleSummonerInputChange = (event) => {
     setSummonerName(event.target.value);
+  };
+
+  const handleOpponentInputChange = (event) => {
+    setOpponentName(event.target.value);
   };
 
   const fetchDataAndMatches = async () => {
     try {
-      const [gameName, tagLine] = summonerName.split("#");
-      const encodedTagLine = encodeURIComponent(tagLine);
+      const [summonerGameName, summonerTagLine] = summonerName.split("#");
+      const [opponentGameName, opponentTagLine] = opponentName.split("#");
 
-      const response = await axios.get(
-        `http://localhost:3001/summonerData/${gameName}/${encodedTagLine}`
+      const encodedSummonerTagLine = encodeURIComponent(summonerTagLine);
+      const encodedOpponentTagLine = encodeURIComponent(opponentTagLine);
+
+      const summonerResponse = await axios.get(
+        `http://localhost:3001/summonerData/${summonerGameName}/${encodedSummonerTagLine}`
       );
 
-      const puuid = response.data.puuid;
-
-      const responseMatch = await axios.get(
-        `http://localhost:3001/summonerMatch/${puuid}`
+      const opponentResponse = await axios.get(
+        `http://localhost:3001/summonerData/${opponentGameName}/${encodedOpponentTagLine}`
       );
 
-      const matchIds = responseMatch.data;
+      const summonerPuuid = summonerResponse.data.puuid;
+      const opponentPuuid = opponentResponse.data.puuid;
 
-      if (matchIds.length === 0) {
-        throw new Error("Brak meczów dla tego przywoływacza.");
+      const summonerMatchResponse = await axios.get(
+        `http://localhost:3001/summonerMatch/${summonerPuuid}`
+      );
+
+      const opponentMatchResponse = await axios.get(
+        `http://localhost:3001/summonerMatch/${opponentPuuid}`
+      );
+
+      const summonerMatchesData = summonerMatchResponse.data;
+      const opponentMatchesData = opponentMatchResponse.data;
+
+      if (
+        summonerMatchesData.length === 0 ||
+        opponentMatchesData.length === 0
+      ) {
+        throw new Error("Brak meczów dla któregoś z przywoływaczy.");
       }
 
-      const matchId = matchIds[0];
-
-      const specificMatchResponse = await axios.get(
-        `http://localhost:3001/specificMatch/${matchId}`
+      const commonMatches = summonerMatchesData.filter((match) =>
+        opponentMatchesData.includes(match)
       );
 
-      const specificMatchData = specificMatchResponse.data;
-      setSpecificMatch(specificMatchData);
-
-      setSummonerData(response.data);
-      setMatches(matchIds);
+      setSummonerData(summonerResponse.data);
+      setOpponentData(opponentResponse.data);
+      // setSummonerMatches(summonerMatchesData);
+      // setOpponentMatches(opponentMatchesData);
+      setCommonMatches(commonMatches);
       setError(null);
     } catch (error) {
-      setError("Błąd pobierania danych przywoływacza: " + error.message);
+      setError("Błąd pobierania danych przywoływaczy: " + error.message);
     }
   };
 
@@ -55,8 +76,14 @@ function App() {
       <input
         type="text"
         value={summonerName}
-        onChange={handleInputChange}
-        placeholder="Wprowadź nazwę użytkownika"
+        onChange={handleSummonerInputChange}
+        placeholder="Wprowadź nazwę użytkownika (Summoner)"
+      />
+      <input
+        type="text"
+        value={opponentName}
+        onChange={handleOpponentInputChange}
+        placeholder="Wprowadź nazwę użytkownika (Przeciwnik)"
       />
       <button onClick={fetchDataAndMatches}>
         Pobierz dane i historię meczy
@@ -64,24 +91,27 @@ function App() {
       {error && <div>{error}</div>}
       {summonerData && (
         <div>
-          <h2>Dane użytkownika:</h2>
+          <h2>Dane użytkownika {summonerName}</h2>
           <pre>{JSON.stringify(summonerData, null, 2)}</pre>
         </div>
       )}
-      {matches && matches.length > 0 && (
+      {opponentData && (
         <div>
-          <h2>Mecze użytkownika:</h2>
-          <ul>
-            {matches.map((match) => (
-              <li key={match}>{match}</li>
-            ))}
-          </ul>
+          <h2>Dane użytkownika {opponentName}</h2>
+          <pre>{JSON.stringify(opponentData, null, 2)}</pre>
         </div>
       )}
-      {specificMatch && (
+      {commonMatches && commonMatches.length > 0 && (
         <div>
-          <h2>Dane meczu:</h2>
-          <pre>{JSON.stringify(specificMatch, null, 2)}</pre>
+          <h2>Wspólne mecze</h2>
+          <ul>
+            {commonMatches.map((match) => (
+              <li key={match}>
+                {match}
+                {" - "}
+              </li>
+            ))}
+          </ul>
         </div>
       )}
     </div>
